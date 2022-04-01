c1ec536cf3629180d3f1c165a2f957843f7fcc8b
franklin <franklinbenjamin032@gmail.com>
2022-04-01 18:51:29 -0400

    add header & book styles

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Book = (props) => {
   const {
-    title, author, delButton, id,
+    title, author, delButton, id, category
   } = props;
+  
+  const progress = 66;
   return (
-    <li className="Book-item">
-      title:
-      {title}
-      {' '}
-      Author:
-      {author}
-      {' '}
-      <button type="button" onClick={() => delButton(id)}> delete</button>
-    </li>
+    <div className="card-container">
+      <div className="book-info">
+        <p className="category">
+          {category}
+        </p>
+        <h2 className="title">{title}</h2>
+        <p className="author">{author}</p>
+        <div className="button-container">
+          <button type="button" className="comments">Comments</button>
+          <button type="button" className="remove" onClick={() => delButton(id)}> delete</button>
+          <button type="button" className="edit">Edit</button>
+        </div>
+      </div>
+      <div className="progress-container">
+        <div className="circle-progress">
+          <CircularProgressbar value={progress} />
+        </div>
+        <div className="percentage-container">
+          <p className="percentage">64%</p>
+          <p className="status">Completed</p>
+        </div>
+      </div>
+      <div className="chapter-container">
+        <p className="chapter-title">CURRENT CHAPTER</p>
+        <p className="chapter-name">Chapter 17</p>
+        <button type="button" className="update-progress">UPDATE PROGRESS</button>
+      </div>
+    </div>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => (
   <nav className="nav">
+    <Link to="/" className="logo">BookStore CMS</Link>
     <div className="link-cont">
-      <Link to="/"> Home </Link>
-      <Link to="/categories"> Categories </Link>
+      <Link to="/" className="link"> Home </Link>
+      <Link to="/categories" className="link"> Categories </Link>
     </div>
+    <Link to="/" className="user"><div className="user-container"><FontAwesomeIcon icon={faUser} /></div></Link>
   </nav>
 );
 
